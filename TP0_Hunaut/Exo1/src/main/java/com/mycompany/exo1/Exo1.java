80a8386926cd350fb1e6b0dee37684ddf4025357
Oscar Hunaut <oscar.hunaut@epfedu.fr>
2023-09-25 10:45:14 +0200

    TP0_Exo 1 Terminé

---
+++ TP0_Hunaut/Exo1/src/main/java/com/mycompany/exo1/Exo1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ * Eleve Oscar Hunaut TDC 17/09
+ */
+public class Exo1 {
+
+    public static void main(String[] args) {
+        String prenom ;
+        Scanner sc ;
+        sc = new Scanner(System.in) ;
+        System.out.println("Hey, ton nom batard ?");
+        prenom = sc.nextLine();
+    }
+}
