5e83e5ed21abe4feeaf148bd1bdabdfb8913eec2
Oscar Hunaut <oscar.hunaut@epfedu.fr>
2023-10-25 00:44:46 +0200

    TP3 Heroic Fantasy - Interface etreVivant - Terminé

---
+++ TP3_Hunaut/TP3_Heritage_Hunaut/src/main/java/com/mycompany/tp3_heritage_hunaut/etreVivant.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.tp3_heritage_hunaut;
+
+/**
+ *
+ * @author Asus
+ */
+public interface etreVivant {
+    
+    void seFatiguer();
+    boolean estVivant();
+    void estAttaqué(int points);   
+}
