f254ba797dad810916189bd088da8f0350b7902a
Oscar Hunaut <oscar.hunaut@epfedu.fr>
2023-11-03 23:59:29 +0100

    Projet LightsOff_Hunaut - Jeu dans sa version initiale complet et fonctionnel Creation de nouvelles methodes et modifications completées : --> Le jeu prend en compte les matrice non carrées pouvant atteindre une taille maximale de 99. --> Création d'un systeme de difficulté qui impacte la taille de la matrice et le mélange de cette matrice (mélange purement aléatoire en mode Hardcore) --> Nouveaux coups disponibles pour les matrices non carrées au niveau des diagonales --> Modification de l'afficahge pour afficher les matrices de grandes tailles

--- TP_LighsOff_Hunaut/LightsOff_Hunaut_version_console/src/lightsoff_hunaut_version_console/GrilleDeCellules.java
+++ TP_LighsOff_Hunaut/LightsOff_Hunaut_version_console/src/lightsoff_hunaut_version_console/GrilleDeCellules.java
@@ -5,6 +5,7 @@
 package lightsoff_hunaut_version_console;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -41,19 +42,36 @@ public class GrilleDeCellules {
     * Oscar Hunaut
     * TDC
     * Mathode qui une affiche la matrice de cellules 
+    * Modification de la fonction toString pour pouvoir afficher les matrices de tailles inferieur a 100
     */
     @Override
     public String toString () { 
+        int L;
+        int K;
+        if (this.Nb_Lignes%2 == 0) {
+            K = (this.Nb_Lignes/2)-1  ;   
+        } else {
+            K = ((this.Nb_Lignes-1)/2)-1  ;
+        }
+        ;
         String CAR ="" ;
-        for (int x = 0 ; x < (this.Nb_Lignes)/2-2 ; x++){
+        if (this.Nb_Lignes>=8){
+            L = 3;
+        } else {
+            L = 2;
+        }
+        for (int x = 0 ; x < (this.Nb_Lignes/2)-K ; x++){
             if (x==0){
                 for (int d=0 ; d<this.Nb_Colonnes+1 ; d++){
                     if (d==0){
                         CAR += ("   |");
-                    }else {
+                    }else if (d<=10){
                         int h = d-1 ;
-                    CAR+=(" " + h + " |");
-                }
+                        CAR+=(" " + h + " |");
+                    } else {
+                        int h = d-1 ;
+                    CAR+=(+ h + " |");
+                    }
                 }    CAR+=("\n");
                 for (int l=0 ; l<this.Nb_Colonnes+1;l++){
                     CAR+=("----");
@@ -67,7 +85,7 @@ public class GrilleDeCellules {
                     int p = h-1;
                     if (h==0){
                         CAR += ("   |");
-                    }else {
+                    }else if (h<=10){
                     CAR+=(" " + p + " |");
                     for (int t = 0; t<this.Nb_Colonnes ; t++){
                         if (this.matriceCellules[d][t].Etat ==  true){
@@ -81,10 +99,24 @@ public class GrilleDeCellules {
                     CAR+=("----");
                 }
             CAR+=("\n");
-            }} 
+            }
+                else {
+                    CAR+=( p + " |");
+                    for (int t = 0; t<this.Nb_Colonnes ; t++){
+                        if (this.matriceCellules[d][t].Etat ==  true){
+                            CAR+=(" X |");
+                        } else {
+                            CAR+=(" O |");
+                        } 
+                }
+                    CAR+=("\n");
+                    for (int l=0 ; l<this.Nb_Colonnes+1;l++){
+                    CAR+=("----");
+                }
+            CAR+=("\n");
+            } 
         }
-        } }   
-    
+        }} }   
     return CAR;
 }
     
@@ -109,6 +141,8 @@ public class GrilleDeCellules {
     * Oscar Hunaut
     * TDC
     * Methode qui active au hasard un ligne un colonne ou une diagonale de la matrice de cellules
+    * Modification : 
+    * --> Modfication de la fonction pour qu'elle puisse traiter les matrices non carrees 
     */
     public void activerLigneColonneOuDiagonaleAleatoire(){
         Random rand = new Random() ;
@@ -132,32 +166,9 @@ public class GrilleDeCellules {
                 }
             }
         } else if (r==2){
-            if (this.Nb_Colonnes==this.Nb_Lignes){
-                for (int c=0; c<this.Nb_Lignes;c++){
-                    if (this.matriceCellules[c][c].Etat == false){
-                    this.matriceCellules[c][c].Etat = true;
-                } else {
-                    this.matriceCellules[c][c].Etat = false;
-                }
-                }
-                
-            } else {
-                System.out.println("Impossible ! La matrice n'est pas carrée !");
-            }
+            this.activerDiagonaleDescendanteAleatoire();
         }else if (r==3){
-            if (this.Nb_Colonnes==this.Nb_Lignes){
-                for (int d=0; d<this.Nb_Lignes;d++){
-                    int D=this.Nb_Colonnes-d-1 ;
-                    if (this.matriceCellules[D][d].Etat == false){
-                    this.matriceCellules[D][d].Etat = true;
-                } else {
-                    this.matriceCellules[D][d].Etat = false;
-                }
-                }
-                
-            } else {
-                System.out.println("Impossible ! La matrice n'est pas carrée !");
-            }
+            this.activerDiagonaleMontanteAleatoire();
         }
     }
     
@@ -203,6 +214,9 @@ public class GrilleDeCellules {
     * Methode qui active la diagonale montante de la matrice de cellules
     */ 
     public void activerDiagonaleMontante(){
+        String Str ;
+        Scanner sc ;
+        sc = new Scanner(System.in);
         if (this.Nb_Colonnes==this.Nb_Lignes){
                 for (int d=0; d<this.Nb_Lignes;d++){
                     int D=this.Nb_Colonnes-d-1 ;
@@ -212,10 +226,41 @@ public class GrilleDeCellules {
                     this.matriceCellules[D][d].Etat = false;
                 }
                 }
+        } else {
+                System.out.println("Laquelle : \n" + "1 ) Celle qui arrive du coin superieur droit\n" + "2 ) Celle qui demarre du coin inferieur gauche");
+            Str = sc.nextLine();
+            if (null==Str) {
+                System.out.println("Choisissez une option parmis celles presentees") ;
+            } else switch (Str) {
+                case "Coin superieur droit ":
+                case "droit":
+                case "1" :
+                    for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Colonnes-d-1;
+                    System.out.println(D);
+                    System.out.println(d);
+                    if (this.matriceCellules[d][D].Etat == false){
+                    this.matriceCellules[d][D].Etat = true;
+                } else {
+                    this.matriceCellules[d][D].Etat = false;
+                }}
+                    break;
+                case "Coin inferieur gauche":
+                case "gauche":
+                case "2":
+                    for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Lignes-d-1 ;
+                    if (this.matriceCellules[D][d].Etat == false){
+                    this.matriceCellules[D][d].Etat = true;
+                } else {
+                    this.matriceCellules[D][d].Etat = false;
+                }
+                }
+                    break;
+                default:
+                    break;
                 
-            } else {
-                System.out.println("Impossible ! La matrice n'est pas carrée !");
-            }
+            }}
     }
     
     /**
@@ -226,6 +271,9 @@ public class GrilleDeCellules {
     * Methode qui active la diagonale descendante de la matrice de cellules
     */ 
     public void activerDiagonaleDescendante(){
+        String Str ;
+        Scanner sc ;
+        sc = new Scanner(System.in);
         if (this.Nb_Colonnes==this.Nb_Lignes){
                 for (int c=0; c<this.Nb_Lignes;c++){
                     if (this.matriceCellules[c][c].Etat == false){
@@ -235,11 +283,41 @@ public class GrilleDeCellules {
                 }
                 }
                 
-            } else {
-                System.out.println("Impossible ! La matrice n'est pas carrée !");
-            }
+        } else {
+                System.out.println("Laquelle : \n" + "1 ) Celle qui arrive du coin inferieur droit\n" + "2 ) Celle qui demarre du coin superieur gauche");
+            Str = sc.nextLine();
+            if (null==Str) {
+                System.out.println("Choisissez une option parmis celles presentees") ;
+            } else switch (Str) {
+                case "Coin inferieur droit ":
+                case "droit":
+                case "1" :
+                    for (int c=0; c<this.Nb_Lignes;c++){
+                    int C = this.Nb_Lignes-c-1;
+                    int D = this.Nb_Colonnes-c-1;
+                    if (this.matriceCellules[C][D].Etat == false){
+                    this.matriceCellules[C][D].Etat = true;
+                } else {
+                    this.matriceCellules[C][D].Etat = false;
+                }}
+                    break;
+                case "Coin superieur gauche":
+                case "gauche":
+                case "2":
+                    for (int c=0; c<this.Nb_Lignes;c++){
+                    if (this.matriceCellules[c][c].Etat == false){
+                    this.matriceCellules[c][c].Etat = true;
+                } else {
+                    this.matriceCellules[c][c].Etat = false;
+                }
+                }
+                    break;
+                default:
+                    break;
+                
+            }}
     }
-    
+
     /**
     *
     * @author Asus
@@ -269,7 +347,9 @@ public class GrilleDeCellules {
     * @author Asus
     * Oscar Hunaut
     * TDC
-    * Methode qui prends en parametre un bombe de tours et melange aléatoirement la matrice de cellules X fois, X etant le nombre de tours 
+    * Methode qui prends en parametre un bombe de tours et melange aléatoirement la matrice de 
+    * cellules X fois, X etant le nombre de tours 
+    * --> Modification : adaptée pour les matrices non carrées
     */ 
     public void melangerMatriceAleatoirement(int nbTours){
         this.eteindreToutesLesCellules();
@@ -277,7 +357,198 @@ public class GrilleDeCellules {
             this.activerLigneColonneOuDiagonaleAleatoire();
         }
     }
+    
+    /**
+    *NON UTILISE
+    * @author Asus
+    * Oscar Hunaut
+    * TDC
+    * Methode qui fonctionne que pour les matrices non carrées ;
+    * Active soit la diagonale montante qui commence au coin inferieur gauche de la matrice 
+    * soit la diagonale qui arrive au coin superieur droit de la matrice
+    * --> methode implémentée dans la methode activerDiagonaleMontante pour la completer et l adapter
+    * aux matrices non carrées 
+    */ 
+    public void activerDiagonaleMontanteMatriceNonCarree(){
+        String Str ;
+        Scanner sc ;
+        sc = new Scanner(System.in);
+        if (this.Nb_Colonnes!=this.Nb_Lignes){
+            System.out.println("Laquelle : \n" + "1 ) Celle qui arrive du coin superieur droit\n" + "2 ) Celle qui demarre du coin inferieur gauche");
+            Str = sc.nextLine();
+            if (null==Str) {
+                System.out.println("Choisissez une option parmis celles presentees") ;
+            } else switch (Str) {
+                case "Coin superieur droit ":
+                case "droit":
+                case "1" :
+                    for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Colonnes-d-1;
+                    System.out.println(D);
+                    System.out.println(d);
+                    if (this.matriceCellules[d][D].Etat == false){
+                    this.matriceCellules[d][D].Etat = true;
+                } else {
+                    this.matriceCellules[d][D].Etat = false;
+                }}
+                    break;
+                case "Coin inferieur gauche":
+                case "gauche":
+                case "2":
+                    for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Lignes-d-1 ;
+                    if (this.matriceCellules[D][d].Etat == false){
+                    this.matriceCellules[D][d].Etat = true;
+                } else {
+                    this.matriceCellules[D][d].Etat = false;
+                }
+                }
+                    break;
+                default:
+                    break;
+                
+            }} else {
+                System.out.println("Impossible ! La matrice est carree !");
+            }
+    }
+    
+     /**
+    *NON UTILISEE
+    * @author Asus
+    * Oscar Hunaut
+    * TDC
+    * Methode qui fonctionne que pour les matrices non carrées ;
+    * Active soit la diagonale descendante qui commence au coin superieur gauche de la matrice 
+    * soit la diagonale qui arrive au coin inferieur droit de la matrice
+    * --> methode implémentée dans la methode activerDiagonaleDescendante pour la completer et l adapter
+    * aux matrices non carrées 
+    */
+    public void activerDiagonaleDescendanteMatriceNonCarree(){
+        String Str ;
+        Scanner sc ;
+        sc = new Scanner(System.in);
+        if (this.Nb_Colonnes!=this.Nb_Lignes){
+            System.out.println("Laquelle : \n" + "1 ) Celle qui arrive du coin inferieur droit\n" + "2 ) Celle qui demarre du coin superieur gauche");
+            Str = sc.nextLine();
+            if (null==Str) {
+                System.out.println("Choisissez une option parmis celles presentees") ;
+            } else switch (Str) {
+                case "Coin inferieur droit ":
+                case "droit":
+                case "1" :
+                    for (int c=0; c<this.Nb_Lignes;c++){
+                    int C = this.Nb_Lignes-c-1;
+                    int D = this.Nb_Colonnes-c-1;
+                    if (this.matriceCellules[C][D].Etat == false){
+                    this.matriceCellules[C][D].Etat = true;
+                } else {
+                    this.matriceCellules[C][D].Etat = false;
+                }}
+                    break;
+                case "Coin superieur gauche":
+                case "gauche":
+                case "2":
+                    for (int c=0; c<this.Nb_Lignes;c++){
+                    if (this.matriceCellules[c][c].Etat == false){
+                    this.matriceCellules[c][c].Etat = true;
+                } else {
+                    this.matriceCellules[c][c].Etat = false;
+                }
+                }
+                    break;
+                default:
+                    break;
+                
+            }} else {
+                System.out.println("Impossible ! La matrice est carree !");
+            }
 }
+    
+    /**
+    *
+    * @author Asus
+    * Oscar Hunaut
+    * TDC
+    * Reprise de la methode activerDiagonaleDescendante afin 
+    * de creer un melange selon les diagonales descendates d'une matrice aléatoirememnt
+    */ 
+    public void activerDiagonaleDescendanteAleatoire(){
+        int A ;
+        Random rand = new Random();
+        if (this.Nb_Colonnes==this.Nb_Lignes){
+                for (int c=0; c<this.Nb_Lignes;c++){
+                    if (this.matriceCellules[c][c].Etat == false){
+                    this.matriceCellules[c][c].Etat = true;
+                } else {
+                    this.matriceCellules[c][c].Etat = false;
+                }
+                }
+                
+        } else {
+            A = rand.nextInt(1);
+            if (A==0) {
+                for (int c=0; c<this.Nb_Lignes;c++){
+                    int C = this.Nb_Lignes-c-1;
+                    int D = this.Nb_Colonnes-c-1;
+                    if (this.matriceCellules[C][D].Etat == false){
+                    this.matriceCellules[C][D].Etat = true;
+                } else {
+                    this.matriceCellules[C][D].Etat = false;
+                }}
+            } else {   
+                for (int c=0; c<this.Nb_Lignes;c++){
+                    if (this.matriceCellules[c][c].Etat == false){
+                    this.matriceCellules[c][c].Etat = true;
+                } else {
+                    this.matriceCellules[c][c].Etat = false;
+                }
+                }}}
+    }
+    
+    /**
+    *
+    * @author Asus
+    * Oscar Hunaut
+    * TDC
+    * Reprise de la methode activerDiagonaleMontante afin 
+    * de creer un melange selon les diagonales montantes d'une matrice aléatoirememnt
+    */
+    public void activerDiagonaleMontanteAleatoire(){
+        int A ;
+        Random rand = new Random();
+        if (this.Nb_Colonnes==this.Nb_Lignes){
+                for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Colonnes-d-1 ;
+                    if (this.matriceCellules[D][d].Etat == false){
+                    this.matriceCellules[D][d].Etat = true;
+                } else {
+                    this.matriceCellules[D][d].Etat = false;
+                }
+                }
+        } else {
+            A = rand.nextInt(1);
+            if (A==0) {
+                    for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Colonnes-d-1;
+                    System.out.println(D);
+                    System.out.println(d);
+                    if (this.matriceCellules[d][D].Etat == false){
+                    this.matriceCellules[d][D].Etat = true;
+                } else {
+                    this.matriceCellules[d][D].Etat = false;
+                }}
+            } else {
+                    for (int d=0; d<this.Nb_Lignes;d++){
+                    int D=this.Nb_Lignes-d-1 ;
+                    if (this.matriceCellules[D][d].Etat == false){
+                    this.matriceCellules[D][d].Etat = true;
+                } else {
+                    this.matriceCellules[D][d].Etat = false;
+                }
+                }
+            }}}
+}
+
 
 
 
