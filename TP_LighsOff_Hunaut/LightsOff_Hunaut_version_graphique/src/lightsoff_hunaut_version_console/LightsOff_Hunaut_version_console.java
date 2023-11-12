7a99d05bcdd9e48b7fb19c305dd2d16fe288d91b
Oscar Hunaut <oscar.hunaut@epfedu.fr>
2023-11-12 17:23:39 +0100

    Projet LightsOff_Hunaut - Partie Graphique complete

---
+++ TP_LighsOff_Hunaut/LightsOff_Hunaut_version_graphique/src/lightsoff_hunaut_version_console/LightsOff_Hunaut_version_console.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightsoff_hunaut_version_console;
+
+/**
+ *
+ * @author Asus
+ */
+public class LightsOff_Hunaut_version_console {
+
+    /**
+     * @param args the command line arguments
+     *  
+     *  BETA.JEU();
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Partie BETA = new Partie();
+        BETA.initialiserPartie2();
+        BETA.lancerPartie();
+    }
+}
